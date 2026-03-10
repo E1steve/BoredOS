4cb6d09b17a14575e48e571830f4f814d03b0992
Jamie Swaim <jamie.c.swaim@gmail.com>
2026-03-10 13:41:05 -0500

    Exposed function to add interrupt to table

--- src/interrupts/interrupts.c
+++ src/interrupts/interrupts.c
@@ -18,15 +18,6 @@ typedef struct {
     uint16_t offset_high;
 } __attribute__((packed)) gate_descriptor;
 
-#define GATE_PRESENT 1 << 7
-#define GATE_DPL_KERNEL 0 << 6
-#define GATE_DPL_USER 3 << 6
-
-#define GATE_TYPE_TASK 0x5
-#define GATE_TYPE_INTERRUPT 0xE
-#define GATE_TYPE_TRAP 0xF
-
-
 static idtr idt_reg;
 static gate_descriptor idt[255];
 
@@ -65,18 +56,16 @@ void program_pic(){
     outb(0xa1, 0xFF);
 }
 
-__attribute__((interrupt)) 
-void keyboard(void* something){
-    char scancode = inb(0x60);
-    write_char(scancode);
-    write_char('\n');
+__attribute__((interrupt))
+void test(void* the_stack){
     outb(0x20, 0x20);
+    return;
 }
 
 void create_idt(){
     program_pic();
 
-    add_int_handler(0x21, (uint32_t)&keyboard, GATE_TYPE_INTERRUPT | GATE_PRESENT | GATE_DPL_KERNEL, 0x8);
+    add_int_handler(0x20, (uint32_t)&test, GATE_TYPE_INTERRUPT | GATE_PRESENT | GATE_DPL_KERNEL, 0x8);
 
     idt_reg.size = sizeof(gate_descriptor)*256;
     idt_reg.offset = (uint32_t)idt;
--- src/interrupts/interrupts.h
+++ src/interrupts/interrupts.h
@@ -1,6 +1,18 @@
 #ifndef INTERRUPTS_H
 #define INTERRUPTS_H
 
+#include <stdint.h>
+
 void create_idt();
+
+#define GATE_PRESENT 1 << 7
+#define GATE_DPL_KERNEL 0 << 6
+#define GATE_DPL_USER 3 << 6
+
+#define GATE_TYPE_TASK 0x5
+#define GATE_TYPE_INTERRUPT 0xE
+#define GATE_TYPE_TRAP 0xF
+
+void add_int_handler(uint8_t index, uint32_t offset, uint8_t flags, uint16_t selector);
 
 #endif
